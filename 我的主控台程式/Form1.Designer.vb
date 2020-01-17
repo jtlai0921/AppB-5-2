d9aaa73a080f8f6dca5bc34e6bf290277be99554
賴紀廷 <49492695+jtlai0921@users.noreply.github.com>
2020-01-17 16:54:05 +0800

    Initial commit

---
+++ 我的主控台程式/Form1.Designer.vb
@@ -0,0 +1,55 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtOutput
+        '
+        Me.txtOutput.BackColor = System.Drawing.SystemColors.ControlText
+        Me.txtOutput.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtOutput.Font = New System.Drawing.Font("新細明體", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.txtOutput.ForeColor = System.Drawing.SystemColors.Control
+        Me.txtOutput.Location = New System.Drawing.Point(0, 0)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtOutput.Size = New System.Drawing.Size(334, 162)
+        Me.txtOutput.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(334, 162)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Name = "Form1"
+        Me.Text = "我的主控台程式"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtOutput As System.Windows.Forms.TextBox
+
+End Class
---
+++ 我的主控台程式/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        ' 電腦和應用程式資訊
+        txtOutput.Text = "電腦名稱: " & My.Computer.Name & vbNewLine
+        txtOutput.Text &= "系統時間: " & My.Computer.Clock.LocalTime & vbNewLine
+        txtOutput.Text &= "程式名稱: " & My.Application.Info.AssemblyName & vbNewLine
+        txtOutput.Text &= "程式標題: " & My.Application.Info.Title & vbNewLine
+        txtOutput.Text &= "程式路徑: " & My.Application.Info.DirectoryPath & vbNewLine
+        ' 文字檔案處理
+        Dim str As String
+        My.Computer.FileSystem.WriteAllText("Test.txt", "Visual Basic程式設計", False)
+        str = My.Computer.FileSystem.ReadAllText("Test.txt")
+        txtOutput.Text &= "Test.txt檔案內容: " & str & vbNewLine
+        ' 測試網路連線
+        If My.Computer.Network.IsAvailable Then
+            txtOutput.Text &= "電腦現在有網路連線..." & vbNewLine
+        End If
+        ' 播放音效
+        My.Computer.Audio.PlaySystemSound(Media.SystemSounds.Beep)
+        txtOutput.SelectionStart = 0
+    End Sub
+End Class
